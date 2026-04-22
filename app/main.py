6537bcf944e3838e2c68e45df97296f5be194a67
Alexandros Souroullas <alexsouroullas@hotmail.gr>
2026-04-22 12:18:48 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 import socket  # noqa: F401
+import threading
 
+def handle_client(connection):
+        """Handles a single client"""
+        with connection:
+            while data := connection.recv(4096):
+                _ = data
+                connection.sendall(b"+PONG\r\n") # Sends response to client
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,12 +15,13 @@ def main():
     # Uncomment the code below to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection,_ = server_socket.accept() # wait for client
 
-    with connection:
-        while data := connection.recv(4096):
-            _ = data
-            connection.sendall(b"+PONG\r\n") # Sends response to client
+    while True:
+        connection,_ = server_socket.accept() # wait for client
+        client_threading = threading.Thread(target=handle_client, args=(connection,))
+        client_threading.start()
+
+
 
 if __name__ == "__main__":
     main()
